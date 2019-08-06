8c94123066b1320a20c9cb02fa173db16a57d4c3
strawhatcode <strawhat7614@gmail.com>
2019-08-06 20:59:12 +0800

    springcloud-sleuth+zipkin服务追踪

--- 08_spring_cloud/eureka-provider/src/main/java/com/example/eureka_provider/controller/helloController.java
+++ 08_spring_cloud/eureka-provider/src/main/java/com/example/eureka_provider/controller/helloController.java
@@ -1,6 +1,9 @@
 package com.example.eureka_provider.controller;
 
 import com.example.eureka_provider.service.HelloService;
+import com.sun.deploy.panel.ExceptionListDialog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class helloController {
+    private static Logger log = LoggerFactory.getLogger(helloController.class);
     @Value("${server.port}")
     String port;
 
@@ -19,6 +23,7 @@ public class helloController {
 
     @GetMapping("/get")
     public String hello(){
+        log.info("这是服务提供者【{}】", port);
         return "来自端口号为["+port+"]provider端返回的"+helloService.hello("[provider内容]");
     }
 }
--- 08_spring_cloud/eureka_consumer/src/main/java/com/example/eureka_consumer/controller/sayHelloController.java
+++ 08_spring_cloud/eureka_consumer/src/main/java/com/example/eureka_consumer/controller/sayHelloController.java
@@ -1,18 +1,21 @@
 package com.example.eureka_consumer.controller;
 
 import com.example.eureka_consumer.service.helloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class sayHelloController {
-
+    private static Logger log = LoggerFactory.getLogger(sayHelloController.class);
     @Autowired
     helloService helloService;
 
     @GetMapping("/getconsumer")
     public String say(){
+        log.info("这是服务消费者！！");
         return "[consumer端请求]-->"+helloService.sayhello();
     }
 }
